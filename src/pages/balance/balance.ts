774d59b11508343fff15c0c9d0036eacaf8f966a
Andre Macedo <dee.xd@hotmail.com>
2019-05-31 12:01:24 -0300

    Balance Page

--- src/pages/balance/balance.ts
+++ src/pages/balance/balance.ts
@@ -1,16 +1,52 @@
-import { NavController, NavParams } from 'ionic-angular'
+import { LoadingController, NavController, NavParams } from 'ionic-angular'
 
 import { Component } from '@angular/core'
+import { Http, Response } from '@angular/http'
 
 @Component({
   selector: 'page-balance',
   templateUrl: 'balance.html'
 })
-export class BalancePage {
 
-  constructor(public navCtrl: NavController, private navParams: NavParams) {
-    let client = navParams.get('client')
-    console.log(client)
+export class BalancePage {
+  client
+  constructor(
+    public navCtrl: NavController,
+    private navParams: NavParams,
+    private http: Http,
+    private loadingController: LoadingController, ) {
+
+    this.client = navParams.get('client')
   }
 
+  async seeTransactionHistory() {
+    const loadingElement = await this.loadingController.create({
+      content: 'Por favor, aguarde...',
+      spinner: 'crescent',
+    })
+    await loadingElement.present();
+    const url = 'http://3.211.115.166/api/transactions/?format=json&client_id=' + this.client.id
+
+    this.http.get(url).subscribe(async (result: Response) => {
+      const body = result.json()
+      this.navCtrl.push(BalancePage, { client: body.results })
+
+      loadingElement.dismiss()
+    })
+  }
+
+  async update() {
+    const loadingElement = await this.loadingController.create({
+      content: 'Por favor, aguarde...',
+      spinner: 'crescent',
+    })
+    await loadingElement.present();
+    const url = 'http://3.211.115.166/api/clients/?format=json&cpf=' + this.client.cpf
+
+    this.http.get(url).subscribe(async (result: Response) => {
+      const body = result.json()
+      this.client = body.results[0]
+      loadingElement.dismiss()
+    })
+  }
 }
